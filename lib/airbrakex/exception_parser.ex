578d7f245bac184874ccf860d66d821bc3c5c4a8
Simon Jesenko <simon.jesenko@gmail.com>
2019-06-20 09:10:44 +0200

    Fix stacktrace parsing when no line number given

--- lib/airbrakex/exception_parser.ex
+++ lib/airbrakex/exception_parser.ex
@@ -3,48 +3,62 @@ defmodule Airbrakex.ExceptionParser do
 
   def parse(kind, reason, stack) do
     {type, message} = info(kind, reason)
+
     %{
       type: type,
       message: message,
       backtrace: stacktrace(stack)
     }
   end
+
   def parse(kind, reason) do
-     parse(kind, reason, System.stacktrace)
+    parse(kind, reason, System.stacktrace())
   end
 
   # from plug/debugger.ex
   defp info(:error, error),
     do: {inspect(error.__struct__), Exception.message(error)}
+
   defp info(:throw, thrown),
     do: {"unhandled throw", inspect(thrown)}
+
   defp info(:exit, reason),
     do: {"unhandled exit", Exception.format_exit(reason)}
 
-
   defp stacktrace(stacktrace) do
-    Enum.map stacktrace, fn
-      ({ module, function, args, [] }) ->
+    Enum.map(stacktrace, fn
+      {module, function, args, []} ->
         %{
           file: "unknown",
           line: 0,
-          function: "#{ module |> Utils.strip_elixir_prefix }.#{ function }#{ args(args) }"
+          function: "#{module |> Utils.strip_elixir_prefix()}.#{function}#{args(args)}"
         }
-      ({ module, function, args, [file: file, line: line_number] }) ->
+
+      {module, function, args, [file: file, line: line_number]} ->
         %{
-          file: "(#{module |> Utils.strip_elixir_prefix }) #{List.to_string(file)}",
+          file: "(#{module |> Utils.strip_elixir_prefix()}) #{List.to_string(file)}",
           line: line_number,
           function: "#{function}#{args(args)}"
         }
-    end
+
+      {module, function, args, [file: file]} ->
+        %{
+          file: List.to_string(file),
+          line: 0,
+          function: "#{function}#{args(args)}"
+        }
+    end)
   end
 
   defp args(args) when is_integer(args) do
     "/#{args}"
   end
+
   defp args(args) when is_list(args) do
-    "(#{args
-        |> Enum.map(&(inspect(&1)))
-        |> Enum.join(", ")})"
+    "(#{
+      args
+      |> Enum.map(&inspect(&1))
+      |> Enum.join(", ")
+    })"
   end
 end
